d161b704ab073a9a41bddd714c1b96837da959df
Rajesh Khatwa <rajesh.kumar.khatwa@sap.com>
2020-04-06 10:26:22 +0530

    modfied comment

--- src/main/java/com/example/demo/sayHello.java
+++ src/main/java/com/example/demo/sayHello.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class sayHello {
 	@RequestMapping("/h")
 	public String HelloWorld() {
-		return "Hi";
+		return "Hi modified";
 	}
 
 }
